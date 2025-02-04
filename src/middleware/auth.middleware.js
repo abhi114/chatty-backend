c4ec92472b0976d61ababe1b75f320e2041f8356
abhi114 <nabhi531@gmail.com>
2025-02-04 21:07:56 +0530

    update profile completed

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,23 +3,25 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req,res,next)=>{
     try {
-        const token = req.cookies.jwt;
-        if(!token){
-            return res.status(401).json({message:"Unauthorized - No Token Provided"});
-        }
-        const decoded = jwt.verify(token,process.env.JWT_SECRET); //decode the cookie using the same key it was encoded;
-        //if it is a false value then it is invalid
-        if(!decoded){
-            return res
-              .status(401)
-              .json({ message: "Unauthorized - Invalid Token" });
-        }
-        const user = await User.findById(decoded.userId).select("-password"); //select everything except the password 
-        if(!user){
-            return res.status(404).json({message:"User not found"});
-        }
-        req.user = user
-        next()
+      const token = req.cookies.jwt;
+      if (!token) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - No Token Provided" });
+      }
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); //decode the cookie using the same key it was encoded;
+      //if it is a false value then it is invalid
+      if (!decoded) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - Invalid Token" });
+      }
+      const user = await User.findById(decoded.userId).select("-password"); //select everything except the password
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      req.user = user; //this sets the user to req which will be used by updateProfile
+      next();
     } catch (error) {
         console.log("error in protect Route middleware",error.message);
         res.status(500).json({message:"Internal Server Error"});
